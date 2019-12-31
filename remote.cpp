8ff60f38135350c617136c9d38fe2ca3fb830b94
guocheng <gc1365798@gmail.com>
2019-12-31 23:39:40 +0800

    chapter 15

---
+++ remote.cpp
@@ -0,0 +1,36 @@
+#include "remote.h"
+#include "tv.h"
+
+
+bool Remote::volup(Tv& t) {
+	return t.volup();
+}
+
+bool Remote::voldown(Tv& t) {
+	return t.voldown();
+}
+
+void Remote::onoff(Tv& t) {
+	t.onoff();
+}
+
+void Remote::chanup(Tv& t) {
+	t.chanup();
+}
+
+void Remote::chandown(Tv& t) {
+	t.chandown();
+}
+
+void Remote::set_mode(Tv& t) {
+	t.set_mode();
+}
+
+void Remote::set_input(Tv& t) {
+	t.set_input();
+}
+
+void Remote::set_chan(Tv& t, int c) {
+	t.channel = c;
+	t.demo();
+}
